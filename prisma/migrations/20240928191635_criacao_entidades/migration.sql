a6ebd966150514584a9c8717c3656f679fd5c954
talitaambrozio <talitalmeidas@gmail.com>
2024-09-28 17:24:05 -0300

    inicio projeto popula banco

---
+++ prisma/migrations/20240928191635_criacao_entidades/migration.sql
@@ -0,0 +1,88 @@
+-- CreateTable
+CREATE TABLE `instituicao` (
+    `id_instituicao` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome_instituicao` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id_instituicao`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `localidade` (
+    `id_localidade` INTEGER NOT NULL AUTO_INCREMENT,
+    `cidade` VARCHAR(191) NOT NULL,
+    `estado` VARCHAR(191) NOT NULL,
+    `pais` VARCHAR(191) NOT NULL,
+    `cep` VARCHAR(191) NOT NULL,
+    `bairro` VARCHAR(191) NOT NULL,
+    `logradouro` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id_localidade`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `agencia` (
+    `id_agencia` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome_agencia` VARCHAR(191) NOT NULL,
+    `numero_agencia` INTEGER NOT NULL,
+    `id_instituicao` INTEGER NOT NULL,
+    `id_localidade` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id_agencia`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cliente` (
+    `id_cliente` INTEGER NOT NULL AUTO_INCREMENT,
+    `tipo_cliente` VARCHAR(191) NOT NULL,
+    `cpf` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `sobrenome` VARCHAR(191) NOT NULL,
+    `sexo` VARCHAR(191) NOT NULL,
+    `razao_social` VARCHAR(191) NOT NULL,
+    `cnpj` VARCHAR(191) NOT NULL,
+    `id_localidade` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id_cliente`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `conta` (
+    `id_conta` INTEGER NOT NULL AUTO_INCREMENT,
+    `numero_conta` VARCHAR(191) NOT NULL,
+    `digito` VARCHAR(191) NOT NULL,
+    `data_abertura` DATETIME(3) NOT NULL,
+    `id_agencia` INTEGER NOT NULL,
+    `id_cliente` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id_conta`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `transacao` (
+    `id_transacao` INTEGER NOT NULL AUTO_INCREMENT,
+    `data_transacao` DATETIME(3) NOT NULL,
+    `tipo_transacao` VARCHAR(191) NOT NULL,
+    `valor` INTEGER NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `id_conta` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id_transacao`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `agencia` ADD CONSTRAINT `agencia_id_instituicao_fkey` FOREIGN KEY (`id_instituicao`) REFERENCES `instituicao`(`id_instituicao`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `agencia` ADD CONSTRAINT `agencia_id_localidade_fkey` FOREIGN KEY (`id_localidade`) REFERENCES `localidade`(`id_localidade`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `cliente` ADD CONSTRAINT `cliente_id_localidade_fkey` FOREIGN KEY (`id_localidade`) REFERENCES `localidade`(`id_localidade`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `conta` ADD CONSTRAINT `conta_id_agencia_fkey` FOREIGN KEY (`id_agencia`) REFERENCES `agencia`(`id_agencia`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `conta` ADD CONSTRAINT `conta_id_cliente_fkey` FOREIGN KEY (`id_cliente`) REFERENCES `cliente`(`id_cliente`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transacao` ADD CONSTRAINT `transacao_id_conta_fkey` FOREIGN KEY (`id_conta`) REFERENCES `conta`(`id_conta`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,104 @@
+import { connect } from "http2";
+import prisma from "../src/config/database.js";
+import { faker } from "@faker-js/faker";
+
+const INSTITUICAO_QTD = 10; // Ajuste conforme necessário
+const CLIENTE_QTD = 50000; // Número total de clientes a serem criados
+const CONTA_QTD = 50000; // Número total de contas a serem criadas
+const TRANSAÇÃO_QTD = 50000; // Número total de transações a serem criadas
+
+async function main() {
+    // Criar instituições e localidades
+    for (let i = 0; i < INSTITUICAO_QTD; i++) {
+        console.log(`Criando instituição ${i + 1} / ${INSTITUICAO_QTD}`);
+
+        const instituicao = await prisma.instituicao.create({
+            data: {
+                nome_instituicao: faker.company.name(),
+            }
+        });
+
+        const localidade = await prisma.localidade.create({
+            data: {
+                
+                cidade: faker.location.city(),
+                estado: faker.location.state(),
+                pais: faker.location.country(),
+                cep: faker.location.zipCode(),
+                bairro: faker.location.streetAddress(),
+                logradouro: faker.location.street(),
+            }
+        });
+
+        const agencia = await prisma.agencia.create({
+            data: {
+                nome_agencia: faker.company.name(),
+                numero_agencia: faker.number.int(),
+                id_instituicao: instituicao.id_instituicao,
+                id_localidade: localidade.id_localidade,
+            }
+        });
+
+        // Criar clientes para cada instituição
+        for (let j = 0; j < CLIENTE_QTD / INSTITUICAO_QTD; j++) {
+            const tipo_cliente = faker.helpers.arrayElement(['PJ', 'PF']);
+            const clienteData = {
+                tipo_cliente: tipo_cliente,
+                cpf: tipo_cliente === 'PF' ? faker.string.numeric(11) : null,
+                nome: faker.person.firstName(),
+                sobrenome: faker.person.lastName(),
+                sexo: tipo_cliente === 'PF' ? faker.helpers.arrayElement(['Feminino', 'Masculino']) : null,
+                razao_social: tipo_cliente === 'PJ' ? faker.company.name() : null,
+                cnpj: tipo_cliente === 'PJ' ? faker.string.numeric(14) : null,
+                id_localidade: localidade.id_localidade,
+                localidade: {
+                    connect: { id_localidade: localidade.id_localidade }
+                  }
+            };
+
+            const cliente = await prisma.cliente.create({
+                data: clienteData
+            });
+
+            // Criar contas para cada cliente
+            for (let k = 0; k < CONTA_QTD / CLIENTE_QTD; k++) {
+                await prisma.conta.create({
+                    data: {
+                        numero_conta: faker.string.numeric(8),
+                        digito: faker.string.numeric(1),
+                        data_abertura: faker.date.past(),
+                        id_agencia: agencia.id_agencia,
+                        id_cliente: cliente.id_cliente,
+                    }
+                });
+            }
+
+            // Criar transações para cada conta
+            const contas = await prisma.conta.findMany({
+                where: { id_cliente: cliente.id_cliente },
+            });
+
+            for (let l = 0; l < TRANSAÇÃO_QTD / CONTA_QTD; l++) {
+                await prisma.transacao.create({
+                    data: {
+                        data_transacao: faker.date.recent(),
+                        tipo_transacao: faker.helpers.arrayElement(['Depósito', 'Saque']),
+                        valor: faker.number.int({ min: 1, max: 1000 }),
+                        descricao: faker.lorem.sentence(),
+                        id_conta: contas[faker.number.int({ min: 0, max: contas.length - 1 })].id_conta,
+                    }
+                });
+            }
+        }
+    }
+}
+
+main().then(async() => {
+    await prisma.$disconnect();
+}).catch(async (e)=> {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
+
+
